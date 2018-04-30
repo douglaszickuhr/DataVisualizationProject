0f23cfa2dab10b69c62ca04304aecacac146c919
douglaszickuhr <douglas@providentcrm.com>
2018-04-30 23:26:45 +0100

    Test

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 # National College of Ireland
 # Data Visualization Project
 # Interactive Visualization 1
